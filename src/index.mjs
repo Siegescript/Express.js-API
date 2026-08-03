2f3edf957fe340e36bb959db0faf374ef1802fe9
Siegescript_ <143763707+Siegescript@users.noreply.github.com>
2026-08-03 20:47:58 +0800

    feat: create mock data variable

--- src/index.mjs
+++ src/index.mjs
@@ -4,22 +4,24 @@ const app = e();
 
 const PORT = process.env.PORT || 3000;
 
+const mockData = [
+    {id: 1, name: "johndoe"},
+    {id: 2, name: "janedoe"},
+    {id: 3, name: "maxmustermann"},
+    {id: 4, name: "erikamustermann"},
+    {id: 5, name: "jeandupont"},
+    {id: 6, name: "mariorossi"},
+    {id: 7, name: "jankowalski"},
+    {id: 8, name: "juanperez"},
+    {id: 9, name: "johnsmith"},
+    ];
+
 app.get("/", (req, res) => {
     res.status(201).send({msg: "Hello World!"});
 });
 
 app.get("/api/users", (req, res) => {
-    res.send([
-        {id: 1, name: "johndoe"},
-        {id: 2, name: "janedoe"},
-        {id: 3, name: "maxmustermann"},
-        {id: 4, name: "erikamustermann"},
-        {id: 5, name: "jeandupont"},
-        {id: 6, name: "mariorossi"},
-        {id: 7, name: "jankowalski"},
-        {id: 8, name: "juanperez"},
-        {id: 9, name: "johnsmith"},
-    ])
+    res.send(mockData);
 });
 
 app.listen(PORT, () => {
