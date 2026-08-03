1ae2067571e9e4dd52a4160181eab1071f36b7f4
Siegescript_ <143763707+Siegescript@users.noreply.github.com>
2026-08-03 18:42:38 +0800

    feat: create GET requests

--- src/index.mjs
+++ src/index.mjs
@@ -4,6 +4,24 @@ const app = e();
 
 const PORT = process.env.PORT || 3000;
 
+app.get("/", (req, res) => {
+    res.status(201).send({msg: "Hello World!"});
+});
+
+app.get("/api/users", (req, res) => {
+    res.send([
+        {id: 1, name: "johndoe"},
+        {id: 2, name: "janedoe"},
+        {id: 3, name: "maxmustermann"},
+        {id: 4, name: "erikamustermann"},
+        {id: 5, name: "jeandupont"},
+        {id: 6, name: "mariorossi"},
+        {id: 7, name: "jankowalski"},
+        {id: 8, name: "juanperez"},
+        {id: 9, name: "johnsmith"},
+    ])
+});
+
 app.listen(PORT, () => {
     console.log(`Running on Port ${PORT}`);
 })
