94b253f7bae4e2cd7da801fc19accd46e7ca088b
Siegescript_ <143763707+Siegescript@users.noreply.github.com>
2026-08-11 16:02:18 +0800

    refactor: another typo in mock data
    FUCK

--- src/index.mjs
+++ src/index.mjs
@@ -32,7 +32,7 @@ const mockData = [
     {id: 2, first_name: "jane", last_name: "doe", email: "janedoe@example.com"},
     {id: 3, first_name: "max", last_name: "mustermann", email: "mmustermann@example.com"},
     {id: 4, first_name: "erika", last_name: "mustermann", email: "emustermann@example.com"},
-    {id: 5, first_name: "jeandupont", last_name: "mustermann", email: "jdupont@example.com"},
+    {id: 5, first_name: "jean", last_name: "dupont", email: "jdupont@example.com"},
     {id: 6, first_name: "mario", last_name: "rossi", email: "mrossi@example.com"},
     {id: 7, first_name: "jan", last_name: "kowalski", email: "jkowalski@example.com"},
     {id: 8, first_name: "juan", last_name: "perez", email: "jperez@example.com"},
