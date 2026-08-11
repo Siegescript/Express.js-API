8494d98266f293b771b0c831ccd1401c1f3ff030
Siegescript_ <143763707+Siegescript@users.noreply.github.com>
2026-08-11 19:57:37 +0800

    refactor: transfer mock database to models/userModel.mjs

--- src/index.mjs
+++ src/index.mjs
@@ -15,19 +15,6 @@ const loggingMiddleware = (request, response, next) => {
 
 app.use(loggingMiddleware);
 
-// MOCK Database
-const mockData = [
-    {id: 1, first_name: "john", last_name: "doe", email: "johndoe@example.com"},
-    {id: 2, first_name: "jane", last_name: "doe", email: "janedoe@example.com"},
-    {id: 3, first_name: "max", last_name: "mustermann", email: "mmustermann@example.com"},
-    {id: 4, first_name: "erika", last_name: "mustermann", email: "emustermann@example.com"},
-    {id: 5, first_name: "jean", last_name: "dupont", email: "jdupont@example.com"},
-    {id: 6, first_name: "mario", last_name: "rossi", email: "mrossi@example.com"},
-    {id: 7, first_name: "jan", last_name: "kowalski", email: "jkowalski@example.com"},
-    {id: 8, first_name: "juan", last_name: "perez", email: "jperez@example.com"},
-    {id: 9, first_name: "john", last_name: "smith", email: "jsmith@example.com"}
-];
-
 // HELPER MIDDLEWARE
 const resolveIndexById = (request, response, next) => {
     const { params: { id } } = request;
