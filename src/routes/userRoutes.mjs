66746641b929840f0ddcd7a1e31d19e357ebe6fe
Siegescript_ <143763707+Siegescript@users.noreply.github.com>
2026-08-19 21:13:56 +0800

    feat: mount validation schema to patch and put requests

--- src/routes/userRoutes.mjs
+++ src/routes/userRoutes.mjs
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { checkSchema } from "express-validator";
-import { createUserValidationSchema, filterUserValidationSchema } from "../utils/validationSchemas.mjs";
+import { createUserValidationSchema, filterUserValidationSchema, updateUserValidationSchema, patchUserValidationSchema } from "../utils/validationSchemas.mjs";
 import { resolveIndexById } from "../middlewares/resolveUser.mjs";
 import { 
     getUsers,
@@ -17,8 +17,10 @@ router.get("/", checkSchema(filterUserValidationSchema), getUsers);
 router.post("/", checkSchema(createUserValidationSchema), createUser);
 
 router.get("/:id", resolveIndexById, getUserById);
-router.put("/:id", resolveIndexById, updateUser);
-router.patch("/:id", resolveIndexById, patchUser);
+
+router.put("/:id", resolveIndexById, checkSchema(updateUserValidationSchema), updateUser);
+router.patch("/:id", resolveIndexById, checkSchema(patchUserValidationSchema), patchUser);
+
 router.delete("/:id", resolveIndexById, deleteUser);
 
 export default router;
