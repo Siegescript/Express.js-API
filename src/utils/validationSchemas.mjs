8661b302c81c909d90118405ad2495b112259b78
Siegescript_ <143763707+Siegescript@users.noreply.github.com>
2026-08-19 19:28:25 +0800

    feat: creat put and patch validation schemas

--- src/utils/validationSchemas.mjs
+++ src/utils/validationSchemas.mjs
@@ -69,4 +69,49 @@ const filterUserValidationSchema = {
     }
 };
 
-export { createUserValidationSchema, filterUserValidationSchema };
+const updateUserValidationSchema = {
+    first_name: {
+        in: ['body'],
+        notEmpty: { errorMessage: "First name is required for full replacement" },
+        isString: { errorMessage: "First name must be a string" },
+        custom: { options: (val) => !/\d/.test(val), errorMessage: "First name cannot contain numbers" }
+    },
+    last_name: {
+        in: ['body'],
+        notEmpty: { errorMessage: "Last name is required for full replacement" },
+        isString: { errorMessage: "Last name must be a string" },
+        custom: { options: (val) => !/\d/.test(val), errorMessage: "Last name cannot contain numbers" }
+    },
+    email: {
+        in: ['body'],
+        notEmpty: { errorMessage: "Email is required for full replacement" },
+        isEmail: { errorMessage: "Invalid email" }
+    }
+};
+
+const patchUserValidationSchema = {
+    first_name: {
+        in: ['body'],
+        optional: true,
+        isString: { errorMessage: "First name must be a string" },
+        custom: { options: (val) => !/\d/.test(val), errorMessage: "First name cannot contain numbers" }
+    },
+    last_name: {
+        in: ['body'],
+        optional: true,
+        isString: { errorMessage: "Last name must be a string" },
+        custom: { options: (val) => !/\d/.test(val), errorMessage: "Last name cannot contain numbers" }
+    },
+    email: {
+        in: ['body'],
+        optional: true,
+        isEmail: { errorMessage: "Invalid email" }
+    }
+};
+
+export { 
+    createUserValidationSchema, 
+    filterUserValidationSchema,
+    updateUserValidationSchema,
+    patchUserValidationSchema
+};
