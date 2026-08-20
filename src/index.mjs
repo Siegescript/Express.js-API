39be8a359b20a89967ebd6bcabdf645f9c371255
Siegescript_ <143763707+Siegescript@users.noreply.github.com>
2026-08-20 20:41:40 +0800

    fix: fix auth routes import

--- src/index.mjs
+++ src/index.mjs
@@ -3,7 +3,7 @@ import express from "express";
 import session from "express-session";
 import { loggingMiddleware } from "./middlewares/logger.mjs";
 import userRoutes from "./routes/userRoutes.mjs";
-import authRoutes from "./routes/userRoutes.mjs";
+import authRoutes from "./routes/authRoutes.mjs";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
