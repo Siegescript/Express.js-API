1bec641b27d2f3fc13cd6f3cfd79bb6d7435771d
Siegescript_ <143763707+Siegescript@users.noreply.github.com>
2026-08-22 00:22:49 +0800

    feat: add mock passwords to mock database

--- src/models/userModel.mjs
+++ src/models/userModel.mjs
@@ -3,54 +3,63 @@ export const users = [
         id: 1, 
         first_name: "john", 
         last_name: "doe", 
-        email: "johndoe@example.com"
+        email: "johndoe@example.com",
+        password: "password123"
     },
     {
         id: 2, 
         first_name: "jane", 
         last_name: "doe", 
-        email: "janedoe@example.com"
+        email: "janedoe@example.com",
+        password: "password123"
     },
     {
         id: 3, 
         first_name: "max", 
         last_name: "mustermann", 
-        email: "mmustermann@example.com"
+        email: "mmustermann@example.com",
+        password: "password123"
     },
     {
         id: 4, 
         first_name: "erika", 
         last_name: "mustermann", 
-        email: "emustermann@example.com"
+        email: "emustermann@example.com",
+        password: "password123"
     },
     {
         id: 5, 
         first_name: "jean", 
         last_name: "dupont", 
-        email: "jdupont@example.com"
+        email: "jdupont@example.com",
+        password: "password123"
     },
     {
         id: 6, 
         first_name: "mario", 
         last_name: "rossi", 
-        email: "mrossi@example.com"
+        email: "mrossi@example.com",
+        password: "password123"
     },
     {
         id: 7, 
         first_name: "jan", 
         last_name: "kowalski", 
-        email: "jkowalski@example.com"
+        email: "jkowalski@example.com",
+        password: "password123"
     },
     {
         id: 8, 
         first_name: "juan", 
         last_name: "perez", 
-        email: "jperez@example.com"
+        email: "jperez@example.com",
+        password: "password123"
     },
     {
         id: 9, 
         first_name: "john", 
         last_name: "smith", 
-        email: "jsmith@example.com"
+        email: "jsmith@example.com",
+        password: "password123"
     }
 ];
