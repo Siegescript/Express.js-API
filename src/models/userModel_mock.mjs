3b79027c95f6aac9ce74429a800946c7becaf7be
Siegescript_ <143763707+Siegescript@users.noreply.github.com>
2026-08-23 18:06:50 +0800

    feat: create mock users model

---
+++ src/models/userModel_mock.mjs
@@ -0,0 +1,65 @@
+export const users = [
+    {
+        id: 1, 
+        first_name: "john", 
+        last_name: "doe", 
+        email: "johndoe@example.com",
+        password: "password123"
+    },
+    {
+        id: 2, 
+        first_name: "jane", 
+        last_name: "doe", 
+        email: "janedoe@example.com",
+        password: "password123"
+    },
+    {
+        id: 3, 
+        first_name: "max", 
+        last_name: "mustermann", 
+        email: "mmustermann@example.com",
+        password: "password123"
+    },
+    {
+        id: 4, 
+        first_name: "erika", 
+        last_name: "mustermann", 
+        email: "emustermann@example.com",
+        password: "password123"
+    },
+    {
+        id: 5, 
+        first_name: "jean", 
+        last_name: "dupont", 
+        email: "jdupont@example.com",
+        password: "password123"
+    },
+    {
+        id: 6, 
+        first_name: "mario", 
+        last_name: "rossi", 
+        email: "mrossi@example.com",
+        password: "password123"
+    },
+    {
+        id: 7, 
+        first_name: "jan", 
+        last_name: "kowalski", 
+        email: "jkowalski@example.com",
+        password: "password123"
+    },
+    {
+        id: 8, 
+        first_name: "juan", 
+        last_name: "perez", 
+        email: "jperez@example.com",
+        password: "password123"
+    },
+    {
+        id: 9, 
+        first_name: "john", 
+        last_name: "smith", 
+        email: "jsmith@example.com",
+        password: "password123"
+    }
+];
