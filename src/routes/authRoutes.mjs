212fb971feaece9cf02bd17272c6ffeab7d39c12
Siegescript_ <143763707+Siegescript@users.noreply.github.com>
2026-08-31 21:00:21 +0800

    feat: update auth routes for passport-js

--- src/routes/authRoutes.mjs
+++ src/routes/authRoutes.mjs
@@ -1,3 +1,4 @@
+import passport from "passport";
 import { Router } from "express";
 import { checkSchema } from "express-validator";
 import { loginValidationSchema } from "../utils/validationSchemas.mjs";
@@ -9,7 +10,11 @@ import {
 
 const router = Router();
 
-router.post("/login", checkSchema(loginValidationSchema), loginUser);
+router.post("/login", 
+    checkSchema(loginValidationSchema), 
+    passport.authenticate("local"),
+    loginUser
+);
 router.post("/logout", logoutUser);
 router.get("/status", getAuthStatus);
 
