a94b475211b6ac5e037ff54cde5f398f12e28fab
Siegescript_ <143763707+Siegescript@users.noreply.github.com>
2026-09-04 15:42:58 +0800

    feat: password encryption

--- src/models/userModel.mjs
+++ src/models/userModel.mjs
@@ -1,5 +1,6 @@
 import { DataTypes } from "sequelize";
 import { sequelize } from "../config/database.mjs";
+import bcrypt from "bcrypt";
 
 const User = sequelize.define("User", {
     id:{
@@ -26,7 +27,21 @@ const User = sequelize.define("User", {
     }
 },{
     tableName: "users",
-    timestamps: true
+    timestamps: true,
+    hooks: {
+        beforeCreate: async (user) => {
+            if(user.password){
+                const salt = await bcrypt.genSalt(10);
+                user.password = await bcrypt.hash(user.password, salt);
+            }
+        },
+        beforeUpdate: async (user) => {
+            if(user.changed('password')){
+                const salt = await bcrypt.genSalt(10);
+                user.password = await bcrypt.hash(user.password, salt);
+            }
+        }
+    }
 });
 
 export{ User };
