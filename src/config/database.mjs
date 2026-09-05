b687904d08ae0c4074fc0bd07061580c9837f57b
Siegescript_ <143763707+Siegescript@users.noreply.github.com>
2026-09-05 17:52:31 +0800

    refactor: update database initialize

--- src/config/database.mjs
+++ src/config/database.mjs
@@ -1,4 +1,7 @@
 import { Sequelize } from "sequelize";
+import bcrypt from "bcrypt";
+import { User } from "../models/userModel.mjs";
+import { users as mockUsers } from "../models/userModel_mock.mjs"
 
 const sequelize = new Sequelize("expressjs_api", "root", process.env.DATABASE_PASSWORD, {
     host: "localhost",
@@ -6,4 +9,37 @@ const sequelize = new Sequelize("expressjs_api", "root", process.env.DATABASE_PA
     logging: false
 });
 
-export { sequelize };
+const initializeDatabase = async () => {
+    try{
+        await sequelize.authenticate();
+        console.log("> Database connection established successfully.");
+
+        await sequelize.sync({ alter: true });
+        console.log("> Models synchronized.");
+
+        const count = await User.count();
+        if(count === 0){
+            const seedData = await Promise.all(
+                mockUsers.map(async (user) => {
+                    const salt = await bcrypt.genSalt(10);
+                    const hashedPassword = await bcrypt.hash("password123", salt);
+                    
+                    return{
+                        first_name: user.first_name,
+                        last_name: user.last_name,
+                        email: user.email,
+                        password: hashedPassword
+                    };
+                })
+            );
+
+            await User.bulkCreate(seedData);
+            console.log("> Mock users data seeded into MySQL");
+        }
+    }catch(error){
+        console.log("Unable to connect to the database:", error);
+        process.exit(1);
+    }
+}
+
+export { sequelize, initializeDatabase };
